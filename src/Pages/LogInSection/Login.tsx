4e4cb9cadec8be223addeba279c98c41461e9c93
Ibrahim <Ibrahimfeddah149@gmail.com>
2023-08-16 15:08:56 +0300

    Making the Login page

--- src/Pages/LogInSection/Login.tsx
+++ src/Pages/LogInSection/Login.tsx
@@ -1,7 +1,35 @@
+import "./login.css";
+import { MdOutlineMailOutline } from "react-icons/md";
+import { RiLockPasswordLine } from "react-icons/ri";
 const Login = () => {
   return (
     <>
-      <div>New Page</div>
+      <div className="login">
+        <div className="overlay" />
+        <div className="container">
+          <div className="col">
+            <h1>Login Page</h1>
+            <div className="email">
+              <label>
+                Email <MdOutlineMailOutline className="email-icon" />
+              </label>
+              <br />
+              <input type="email" placeholder="Email" />
+            </div>
+            <div className="password">
+              <label>
+                Password <RiLockPasswordLine className="pass-icon" />
+              </label>
+              <br />
+              <input type="password" placeholder="Password" />
+            </div>
+            <button>Submit</button>
+            <p className="register">
+              Don't have account ? <a href="">Register</a>
+            </p>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
